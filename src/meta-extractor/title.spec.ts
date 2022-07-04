50d8d48119027df9beef59e46240441d1bec7dbe
SettingDust <settingdust@gmail.com>
2022-07-04 10:41:17 +0800

    feat(extractor): support separator in title

--- src/meta-extractor/title.spec.ts
+++ src/meta-extractor/title.spec.ts
@@ -3,91 +3,135 @@
 import { expect } from 'chai'
 import title from './title.js'
 import { parseHTML } from 'linkedom'
-import { from, of, pluck, tap } from 'rxjs'
+import {
+  combineLatestAll,
+  from,
+  of,
+  pipe,
+  pluck,
+  switchMap,
+  tap,
+  windowCount
+} from 'rxjs'
+import { map } from 'rxjs/operators'
+import { readFile } from 'fs/promises'
+
+const $title = pipe(
+  map<string, Window & typeof globalThis>(parseHTML),
+  pluck('document'),
+  title,
+  pluck('title')
+)
+
+const $singleTitle = pipe(
+  $title,
+  tap((it) => expect(it).eq('foo'))
+)
 
 describe('meta extractor', () => {
   describe('title', () => {
     it('should read meta og title', function (done) {
-      of(parseHTML('<meta property="og:title" content="foo">'))
-        .pipe(
-          pluck('document'),
-          title,
-          tap((it) => expect(it).to.eq('foo'))
-        )
+      of('<meta property="og:title" content="foo">')
+        .pipe($singleTitle)
         .subscribe(() => done())
     })
 
     it('should read meta twitter title', function (done) {
       from([
-        parseHTML('<meta property="twitter:title" content="foo">'),
-        parseHTML('<meta name="twitter:title" content="foo">')
+        '<meta property="twitter:title" content="foo">',
+        '<meta name="twitter:title" content="foo">'
       ])
-        .pipe(
-          pluck('document'),
-          title,
-          tap((it) => expect(it).to.eq('foo'))
-        )
+        .pipe($singleTitle)
         .subscribe(() => done())
     })
 
     it('should read title tag', function (done) {
-      from([parseHTML('<html lang=""><head><title>foo</title></head></html>')])
-        .pipe(
-          pluck('document'),
-          title,
-          tap((it) => expect(it).to.eq('foo'))
-        )
+      from(['<html lang=""><head><title>foo</title></head></html>'])
+        .pipe($singleTitle)
         .subscribe(() => done())
     })
 
     it('should read post title class', function (done) {
-      from([parseHTML('<div class="post-title">foo</div>')])
-        .pipe(
-          pluck('document'),
-          title,
-          tap((it) => expect(it).to.eq('foo'))
-        )
+      from(['<div class="post-title">foo</div>'])
+        .pipe($singleTitle)
         .subscribe(() => done())
     })
 
     it('should read entry title class', function (done) {
-      from([parseHTML('<div class="entry-title">foo</div>')])
-        .pipe(
-          pluck('document'),
-          title,
-          tap((it) => expect(it).to.eq('foo'))
-        )
+      from(['<div class="entry-title">foo</div>'])
+        .pipe($singleTitle)
         .subscribe(() => done())
     })
 
     it('should read <a> like title', function (done) {
       from([
-        parseHTML('<h1 class="title"><a>foo</a></h1>'),
-        parseHTML('<h1 class="like-title"><a>foo</a></h1>'),
-        parseHTML('<h2 class="like-title"><a>foo</a></h2>')
+        '<h1 class="title"><a>foo</a></h1>',
+        '<h1 class="like-title"><a>foo</a></h1>',
+        '<h2 class="like-title"><a>foo</a></h2>'
       ])
-        .pipe(
-          pluck('document'),
-          title,
-          tap((it) => expect(it).to.eq('foo'))
-        )
+        .pipe($singleTitle)
         .subscribe(() => done())
     })
 
     it('should read text like title', function (done) {
       from([
-        parseHTML('<h1 class="title">foo</h1>'),
-        parseHTML('<h1 class="like-title">foo</h1>'),
-        parseHTML('<h2 class="like-title">foo</h2>')
+        '<h1 class="title">foo</h1>',
+        '<h1 class="like-title">foo</h1>',
+        '<h2 class="like-title">foo</h2>'
       ])
+        .pipe($singleTitle)
+        .subscribe(() => done())
+    })
+
+    it('should with correct priority', function (done) {
+      from(['./test/meta-class-title.html'])
         .pipe(
-          pluck('document'),
-          title,
-          tap((it) => expect(it).to.eq('foo'))
+          switchMap((it) => readFile(it, { encoding: 'utf8' })),
+          $title,
+          map((it) => of(it)),
+          combineLatestAll(),
+          tap((it) =>
+            expect(it).deep.eq([
+              'og',
+              'twitter',
+              'twitter-name',
+              'post-title',
+              'a-title',
+              'class-title',
+              'entry-title',
+              'title-tag'
+            ])
+          )
         )
         .subscribe(() => done())
     })
 
-
+    it('should respect the separator', function (done) {
+      from([
+        '<meta property="og:title" content="foo | bar">',
+        '<meta property="og:title" content="foo - bar">',
+        '<meta property="og:title" content="foo \\ bar">',
+        '<meta property="og:title" content="foo / bar">',
+        '<meta property="og:title" content="foo > bar">',
+        '<meta property="og:title" content="foo » bar">',
+        '<meta property="og:title" content="foo · bar">',
+        '<meta property="og:title" content="foo – bar">'
+      ])
+        .pipe(
+          switchMap((it) => $title(of(it))),
+          windowCount(3),
+          switchMap((it) =>
+            it.pipe(
+              map((it) => of(it)),
+              combineLatestAll(),
+              tap((it) => expect(it).length(3)),
+              tap((it) => expect(it.slice(1)).deep.eq(['foo', 'bar']))
+            )
+          ),
+          map((it) => of(it)),
+          combineLatestAll()
+        )
+        .subscribe(() => done())
+    })
   })
 })
--- src/meta-extractor/title.ts
+++ src/meta-extractor/title.ts
@@ -3,21 +3,62 @@
 
 import { StringMetaExtractor } from './index.js'
 import { $attr, $query, $queryByClass, $text } from './utils.js'
-import { concat, first, Observable, pipe, pluck } from 'rxjs'
+import {
+  concatMap,
+  distinct,
+  from,
+  mergeMap,
+  Observable,
+  pipe,
+  pluck
+} from 'rxjs'
 import isStringBlank from 'is-string-blank'
+import { filter, map } from 'rxjs/operators'
 
 export const extractors: { [key: string]: StringMetaExtractor } = {
   'meta og': pipe($query('meta[property="og:title"]'), $attr('content')),
-  'meta twitter': pipe($query('meta[property="twitter:title"]'), $attr('content')),
-  'meta twitter attr name': pipe($query('meta[name="twitter:title"]'), $attr('content')),
+  'meta twitter': pipe(
+    $query('meta[property="twitter:title"]'),
+    $attr('content')
+  ),
+  'meta twitter attr name': pipe(
+    $query('meta[name="twitter:title"]'),
+    $attr('content')
+  ),
   'post title class': pipe($queryByClass('post-title'), $text),
+  'a like title': pipe($query(':is(h1, h2)[class*="title" i] a'), $text),
+  'h1 h2 like title': pipe(
+    $query('h1[class*="title" i], h2[class*="title" i]'),
+    $text
+  ),
   'entry title class': pipe($queryByClass('entry-title'), $text),
-  'a like title': pipe($query('h1[class*="title" i], h2[class*="title" i] a'), $text),
-  'h1 h2 like title': pipe($query('h1[class*="title" i], h2[class*="title" i]'), $text),
   title: pipe(pluck('title'))
 }
 
 const $operators = (document: Observable<Document>) =>
-  concat(...Object.values(extractors).map((it) => document.pipe(it)))
+  from(Object.values(extractors)).pipe(
+    map((it) => it(document)),
+    concatMap((it) => it)
+  )
 
-export default (document: Observable<Document>) => $operators(document).pipe(first((it) => !isStringBlank(it)))
+const SEPARATORS = ['|', '-', '\\', '/', '>', '»', '·', '–'].map(
+  (it) => ` ${it} `
+)
+
+export default (document: Observable<Document>) =>
+  $operators(document).pipe(
+    filter((it) => !isStringBlank(it)),
+    mergeMap((title) => {
+      const separatorIndex = SEPARATORS.map((it) => title.lastIndexOf(it)).find(
+        (it) => it !== -1
+      )
+      const titles = [title]
+      if (separatorIndex !== undefined) {
+        titles.push(title.slice(0, separatorIndex))
+        titles.push(title.slice(separatorIndex + 3))
+      }
+      return titles
+    }),
+    distinct(),
+    map((title) => ({ title }))
+  )
