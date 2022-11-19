ca63fb66b6c5ecba9247e1c5f989d9640f924467
SettingDust <settingdust@gmail.com>
2022-11-19 13:12:15 +0800

    chore(extractor): add types of `extract`

--- src/meta-extractor/index.ts
+++ src/meta-extractor/index.ts
@@ -11,20 +11,37 @@ import { NestedPartialK, TMerged } from '../utils/types'
 import { dedupe, deepMerge } from '../utils/memoized-functions'
 import { Extractor } from './utils'
 
+type DefaultExtracted = NestedPartialK<ExtractorExtracted<DefaultExtractors>> &
+  TitleExtracted &
+  UrlExtracted
+
+export interface ExtractOptions<T> {
+  url?: string
+  extractors?: Extractor<T, unknown>[]
+}
+
 export async function extract(
-  html: string | Document,
-  inputUrl?: string
-): Promise<
-  NestedPartialK<ExtractorExtracted<DefaultExtractors>> &
-    ExtractorExtracted<typeof titleExtractor> &
-    ExtractorExtracted<typeof urlExtractor>
->
+  html: string | Document
+): Promise<DefaultExtracted>
 
 export async function extract<T>(
   html: string | Document,
-  inputUrl?: string,
-  extractors = <Extractor<T, unknown>[]>defaultExtractors
+  options: Omit<ExtractOptions<T>, 'extractors'>
+): Promise<DefaultExtracted>
+
+export async function extract<T>(
+  html: string | Document,
+  options?: ExtractOptions<T>
+): Promise<NestedPartialK<T & TitleExtracted & UrlExtracted>>
+
+export async function extract<T>(
+  html: string | Document,
+  options: ExtractOptions<T> = {}
 ) {
+  options = {
+    extractors: <Extractor<T, unknown>[]>defaultExtractors,
+    ...options
+  }
   const document =
     typeof html === 'string'
       ? <Document>(<unknown>new DOMParser().parseFromString(html, 'text/html'))
@@ -33,22 +50,22 @@ export async function extract<T>(
   const title = titleExtractor.selector(
     titleExtractor.processor(
       titleExtractor.operators.flatMap((it) => it[1](document)),
-      inputUrl
+      options.url
     ),
     undefined,
-    inputUrl
+    options.url
   )
 
   const url = urlExtractor.selector(
     urlExtractor.processor(
       urlExtractor.operators.flatMap((it) => it[1](document)),
-      inputUrl
+      options.url
     ),
     title.title,
-    inputUrl
+    options.url
   )
 
-  const results = await Promise.all(extractors).then((it) =>
+  const results = await Promise.all(options.extractors).then((it) =>
     it
       .map(({ operators, processor, selector }) => {
         const operated = operators
